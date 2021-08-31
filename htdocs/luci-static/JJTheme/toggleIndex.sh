2fa7aaea9e3af38138f23036db88e5585a4f3c6b
netitgo <netitgo@qq.com>
2021-08-31 21:07:41 +0800

    correct the spelling

--- htdocs/luci-static/JJTheme/toggleIndex.sh
+++ htdocs/luci-static/JJTheme/toggleIndex.sh
@@ -5,7 +5,7 @@ JJ_FILE='index_jj.htm'
 cd $WORKING_PATH
 if [ -f "$ORIGINAL_FILE" ];then
     mv $ORIGINAL_FILE index_backup_by_JJ.htm
-    echo "Backup the original file, DONE!"
+    echo "Backup the original file, Done!"
     if [ -f "$JJ_FILE" ];then
         mv $JJ_FILE index.htm
         echo "Install the JJ theme file, Done!"
